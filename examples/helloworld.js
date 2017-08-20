76f1b0a7af1ac68b50915e19c0c8efba7a3beafb
svey <marcus.svehlak@gmail.com>
2017-08-20 02:42:50 -0700

    Created base model extortion bot

--- examples/helloworld.js
+++ examples/helloworld.js
@@ -1,97 +1,95 @@
-//
-// Copyright (c) 2016 Cisco Systems
-// Licensed under the MIT License 
-//
+const SparkBot = require('node-sparkbot');
+const SparkAPIWrapper = require('node-sparkclient');
 
-/* 
- * a Cisco Spark bot that:
- *   - sends a welcome message as he joins a room, 
- *   - answers to a /hello command, and greets the user that chatted him
- *   - supports /help and a fallback helper message
- *
- * + leverages the "node-sparkclient" library for Bot to Cisco Spark communications.
- * 
- */
+const bot = new SparkBot();
+const spark = new SparkAPIWrapper(process.env.SPARK_TOKEN);
 
-var SparkBot = require("node-sparkbot");
-var bot = new SparkBot();
-bot.interpreter.prefix = "#"; // Remove comment to overlad default / prefix to identify bot commands
-
-var SparkAPIWrapper = require("node-sparkclient");
-if (!process.env.SPARK_TOKEN) {
-    console.log("Could not start as this bot requires a Cisco Spark API access token.");
-    console.log("Please add env variable SPARK_TOKEN on the command line");
-    console.log("Example: ");
-    console.log("> SPARK_TOKEN=XXXXXXXXXXXX DEBUG=sparkbot* node helloworld.js");
-    process.exit(1);
-}
-var spark = new SparkAPIWrapper(process.env.SPARK_TOKEN);
-
-
-//
-// Help and fallback commands
-//
-bot.onCommand("help", function (command) {
-    spark.createMessage(command.message.roomId, "Hi, I am the Hello World bot !\n\nType /hello to see me in action.", { "markdown":true }, function(err, message) {
-        if (err) {
-            console.log("WARNING: could not post message to room: " + command.message.roomId);
-            return;
-        }
-    });
-});
-bot.onCommand("fallback", function (command) {
-    spark.createMessage(command.message.roomId, "Sorry, I did not understand.\n\nTry /help.", { "markdown":true }, function(err, response) {
-        if (err) {
-            console.log("WARNING: could not post Fallback message to room: " + command.message.roomId);
-            return;
-        }
-    });
+bot.onCommand('fallback', (command) => {
+  const {
+    roomId
+  } = command.message;
+  spark.createMessage(roomId, 'There\'s two choices here stranger /bank and /deposit', { markdown: true }, (err) => {
+    if (err) {
+      console.log(`WARNING: could not post Fallback message to room: ${roomId}`);
+      return;
+    }
+  });
 });
 
+bot.onCommand('bank', (command) => {
+  const {
+    //id,
+    roomId,
+    // roomType,
+    // toPersonId,
+    // toPersonEmail,
+    // text,
+    // markdown,
+    // files,
+    // personId,
+    personEmail, // Spark User that created the message orginally 
+    // created,
+    // mentionedPeople
+  } = command.message;
 
-//
-// Bots commands here
-//
-bot.onCommand("hello", function (command) {
-    var email = command.message.personEmail; // Spark User that created the message orginally 
-    spark.createMessage(command.message.roomId, "Hello <@personEmail:" + email + ">", { "markdown":true }, function(err, message) {
-        if (err) {
-            console.log("WARNING: could not post Hello message to room: " + command.message.roomId);
-            return;
-        }
-    });
+  spark.createMessage(roomId, `<@personEmail:${personEmail}> the bank is open!`, { markdown: true }, (err) => {
+    if (err) {
+      console.log(`WARNING: couldn't POST /bank at: ${roomId}`);
+      return;
+    }
+  });
 });
 
+bot.onCommand('deposit', (command) => {
+  const {
+    // id,
+    roomId,
+    // roomType,
+    // toPersonId,
+    // toPersonEmail,
+    // text,
+    // markdown,
+    // files,
+    // personId,
+    personEmail, // Spark User that created the message orginally 
+    // created,
+    // mentionedPeople
+  } = command.message;
 
-//
-// Welcome message 
-// sent as the bot is added to a Room
-//
-bot.onEvent("memberships", "created", function (trigger) {
-    var newMembership = trigger.data; // see specs here: https://developer.ciscospark.com/endpoint-memberships-get.html
-    if (newMembership.personId != bot.interpreter.person.id) {
-        // ignoring
-        console.log("new membership fired, but it is not us being added to a room. Ignoring...");
-        return;
+  spark.createMessage(roomId, `<@personEmail:${personEmail}> thanks for the deposit`, { markdown: true }, (err) => {
+    if (err) {
+      console.log(`WARNING: couldn't POST /deposit at: ${roomId}`);
+      return;
+    }
+  });
+});
+
+bot.onEvent('memberships', 'created', (trigger) => {
+  const {
+    personId,
+    roomId
+  } = trigger.data; // see specs here: https://developer.ciscospark.com/endpoint-memberships-get.html
+  if (personId != bot.interpreter.person.id) {
+    // ignoring
+    console.log('new membership fired, but it is not us being added to a room. Ignoring...');
+    return;
+  }
+
+  console.log(`bot's just added to room: ${roomId}`);
+
+  spark.createMessage(roomId, 'Hi, I am the harmless extortion bot! Type /bank to get started.', { markdown: true }, (err, message) => {
+    if (err) {
+      console.log(`WARNING: could not POST intro message to room at: ${roomId}`);
+      return;
     }
 
-    // so happy to join
-    console.log("bot's just added to room: " + trigger.data.roomId);
-    
-    spark.createMessage(trigger.data.roomId, "Hi, I am the Hello World bot !\n\nType /hello to see me in action.", { "markdown":true }, function(err, message) {
+    if (message.roomType === 'group') {
+      spark.createMessage(roomId, 'Note I will wake up only when @mentionned.', { markdown: true }, (err) => {
         if (err) {
-            console.log("WARNING: could not post Hello message to room: " + trigger.data.roomId);
-            return;
+          console.log(`WARNING: could not post Mention message to room: ${roomId}`);
+          return;
         }
-
-        if (message.roomType == "group") {
-            spark.createMessage(trigger.data.roomId, "**Note that this is a 'Group' room. I will wake up only when mentionned.**", { "markdown":true }, function(err, message) {
-                if (err) {
-                    console.log("WARNING: could not post Mention message to room: " + trigger.data.roomId);
-                    return;
-                }
-            });
-        }      
-    }); 
+      });
+    }
+  });
 });
-
